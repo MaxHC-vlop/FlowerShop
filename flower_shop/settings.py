43f02fc6cb2ee1e0037830696776c8f93c74ec43
Maksim <106922768+Kalininmladshiy@users.noreply.github.com>
2023-01-18 10:44:07 +0300

    Added templates and start page

--- flower_shop/settings.py
+++ flower_shop/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from environs import Env
 
@@ -39,7 +40,7 @@ ROOT_URLCONF = 'flower_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
