7ff10f48fd8ba1669831b9444a5240a751b09143
Vladuslav Dragushin <maxhc@inbox.ru>
2023-01-19 15:49:31 +0300

    Added POST.method for order

--- flowersapp/views.py
+++ flowersapp/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.csrf import csrf_exempt
 
-from flowersapp.models import Bouquet, Buyer, Consultation
+from flowersapp.models import Bouquet, Buyer
+from flowersapp.models import Consultation, Order
+
 
 @csrf_exempt
 def index(request):
     recommended_bouquets = Bouquet.objects.filter(recommend=True)
 
-    if request.method == "POST":
-        full_name = request.POST.get("fname")
-        phonenumber = request.POST.get("tel")
-    
+    if request.method == 'POST':
+        full_name = request.POST.get('fname')
+        phonenumber = request.POST.get('tel')
+
         buyer, created = Buyer.objects.get_or_create(
-            phonenumber=phonenumber, 
+            phonenumber=phonenumber,
             defaults={
                 'full_name': full_name},
         )
@@ -21,39 +23,59 @@ def index(request):
             phonenumber=phonenumber,
         ).buyer.set([buyer])
 
-    return render(request, 'index.html', context={"bouquets": recommended_bouquets})
+    return render(
+        request, 'index.html', context={'bouquets': recommended_bouquets}
+        )
 
 
 def card(request, bouquet_url):
     bouquet = get_object_or_404(Bouquet, slug=bouquet_url)
-    return render(request, 'card.html', context={"bouquet": bouquet})
+    return render(request, 'card.html', context={'bouquet': bouquet})
 
 
 def catalog(request):
     bouquets = Bouquet.objects.all()
-    return render(request, 'catalog.html', context={"bouquets": bouquets})
+    return render(request, 'catalog.html', context={'bouquets': bouquets})
 
 
 @csrf_exempt
 def consultation(request):
-    if request.method == "POST":
-        full_name = request.POST.get("fname")
-        phonenumber = request.POST.get("tel")
-    
+    if request.method == 'POST':
+        full_name = request.POST.get('fname')
+        phonenumber = request.POST.get('tel')
+
         buyer, created = Buyer.objects.get_or_create(
-            phonenumber=phonenumber, 
+            phonenumber=phonenumber,
             defaults={
                 'full_name': full_name},
         )
         Consultation.objects.create(
             full_name=full_name,
             phonenumber=phonenumber,
-        ).buyer.set([buyer])             
+        ).buyer.set([buyer])
 
     return render(request, 'consultation.html')
 
 
+@csrf_exempt
 def order(request):
+    if request.method == "POST":
+        full_name = request.POST.get('fname')
+        phonenumber = request.POST.get('tel')
+        address = request.POST.get('adres')
+        delivery_time = request.POST.get('orderTime')
+
+        buyer, created = Buyer.objects.get_or_create(
+            phonenumber=phonenumber,
+            defaults={
+                'full_name': full_name,
+                'address': address
+            },
+        )
+        Order.objects.create(
+            buyer=buyer,
+            delivery_time=delivery_time,
+        )
     return render(request, 'order.html')
 
 
