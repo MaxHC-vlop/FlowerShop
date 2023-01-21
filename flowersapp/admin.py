5ef60f57913360e8a4a32b632088e272df4eb1f7
killerarz <milov52@gmail.com>
2023-01-21 10:59:04 +0300

    Updated admins fields

--- flowersapp/admin.py
+++ flowersapp/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from flowersapp.models import BouquetQuiz, Buyer, BouquetQuiz, Shop
+from django.utils.html import format_html
+
 from flowersapp.models import Bouquet, Order
+from flowersapp.models import BouquetQuiz, Buyer, Shop
 from flowersapp.models import Consultation, Payment
+
+
+class OrderItemInline(admin.TabularInline):
+    model = Order
+    extra = 0
 
 
 @admin.register(Buyer)
 class BuyerAdminModel(admin.ModelAdmin):
-    pass
+    inlines = [OrderItemInline]
 
 
 @admin.register(Shop)
@@ -16,12 +23,29 @@ class ShopAdminModel(admin.ModelAdmin):
 
 @admin.register(Bouquet)
 class BouquetAdminModel(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("bouquet_name", )}
+    prepopulated_fields = {"slug": ("bouquet_name",)}
+
+    readonly_fields = ["bouquet_image", ]
+
+    def bouquet_image(self, bouquet_image):
+        return format_html(
+            '<img src="/media/{}" height=200px width=auto />', bouquet_image.bouquet_photo
+        )
+
+    bouquet_image.short_description = 'Превью букета'
 
 
 @admin.register(Order)
 class OrderAdminModel(admin.ModelAdmin):
-    pass
+    readonly_fields = ["bouquet_image", ]
+
+    def bouquet_image(self, bouquet_image):
+        return format_html(
+            '<img src="/media/{}" height=200px width=auto />', bouquet_image.bouquet.bouquet_photo
+        )
+
+    bouquet_image.short_description = 'Превью букета'
+
 
 
 @admin.register(Consultation)
@@ -32,6 +56,7 @@ class ConsultationAdminModel(admin.ModelAdmin):
 @admin.register(Payment)
 class PaymentAdminModel(admin.ModelAdmin):
     pass
+
 
 @admin.register(BouquetQuiz)
 class BouquetQuizAdminModel(admin.ModelAdmin):
